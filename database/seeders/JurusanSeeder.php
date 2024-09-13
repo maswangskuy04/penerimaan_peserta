0c9f6f691c3ef85482c9169cea05c3e6589eb22d
maswangskuy04 <reyhanmarlizal@gmail.com>
2024-09-13 23:00:15 +0700

    hhh

---
+++ database/seeders/JurusanSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class JurusanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $jurusans = [
+            'Operator Komputer',
+            'Bahasa Inggris',
+            'Desain Grafis',
+            'Tata Boga',
+            'Tata Busana',
+            'Teknik Pendingin',
+            'Teknik Komputer',
+            'Otomotif Sepeda Motor',
+            'Jaringan Komputer',
+            'Barista',
+            'Bahasa Korea',
+            'Makeup Artist',
+            'Video Editor',
+            'Content Creator',
+            'Web Programming'
+        ];
+
+        foreach($jurusans as $jurusan) {
+            DB::table('jurusans')->insert([
+                'nama_jurusan' => $jurusan
+            ]);
+        }
+    }
+}
